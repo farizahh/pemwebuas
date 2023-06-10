d505098826dc29e204f844dad0dbdc111c24609b
farizahh <90184606+farizahh@users.noreply.github.com>
2023-06-10 18:23:25 +0700

    Create index.php

---
+++ assets/index.php
@@ -0,0 +1,28 @@
+<?php
+	include "../koneksi.php";
+	
+?>
+
+<html>
+	<head>
+		<meta charset="uft-8">
+		<title>Halaman Admin Bakso Tekwoleh</title>
+		<link rel="stylesheet" href="../assets/boxicons/css/boxicons.min.css">
+		<link rel="stylesheet" href="../style.css">
+		<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+	</head>
+		
+	<body>
+		<div class="row">
+			<div class="col col-3">
+				<?php include("navbar.php"); ?>
+			</div>
+			<div class="col col-9">
+					Dashboard
+				
+			</div>
+		</div>
+	
+
+  </body>
+</html>
